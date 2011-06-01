3f4d7c271e4a8738336e13fd08f93fe82a36eac2
Jordi Fernàndez <jordifernandez@Jordi-Fernandezs-MacBook.local>
2011-06-01 13:20:19 +0200

    Fixed bug for stdactions

--- widgets/fstdactions.h
+++ widgets/fstdactions.h
@@ -42,8 +42,8 @@ class ST_WIDGETS_EXPORT FAction : public QAction
 public:
 	enum EnActionId
 	{
-	    FAcceptActionId,
-	    FCancelActionId,
+		FAcceptActionId = QDialog::Accepted,
+		FCancelActionId = QDialog::Rejected,
 	    FEditActionId,
 	    FEditAddActionId,
 	    FEditDeleteActionId,
